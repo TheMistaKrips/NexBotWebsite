51702994b8c4cc2d8479df2c7cb36b1e92556dfd
KripsaKrips <iteaunity@gmail.com>
2024-08-23 21:22:29 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Navigation from "./components/Navigation";
 import './App.css'
 import SplineTool from "./components/Spline";
-import Details from "./components/Details";
 
 function App() {
   return (
     <div className="flex flex-row ">
       <Navigation />
       <SplineTool />
-      <Details />
     </div>
   );
 }
